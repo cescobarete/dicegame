6702d195bad88c30315681373076c229eaf04ac9
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-02-07 22:44:12 -0600

    hj

--- myjs.js
+++ myjs.js
@@ -1,4 +1,4 @@
 //Roll dice for a game
 function RollDie() {
-    document.getElementByID("dice").value = Math.floor(Math.random()*6) + 1;
+    document.getElementById("Dice").value = Math.floor(Math.random()*6) + 1;
 }
